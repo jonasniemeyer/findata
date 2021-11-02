d20ad126b6631b171f603d3cc436c9d5e5b55ced
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2021-11-02 10:55:29 +0100

    make sec filings importable

--- __init__.py
+++ __init__.py
@@ -3,4 +3,9 @@ from finance_data.macrotrends import MacrotrendsReader
 from finance_data.french import FrenchReader
 from finance_data.fred import FREDReader
 from finance_data.msci import MSCIReader
+from finance_data.sec import (
+    Filing13D,
+    Filing13G,
+    Filing13F
+)
 from finance_data.functions import margin_debt
