4037b9bb9a2b892ad4eae3dd975e2df1761756d9
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2021-12-19 15:35:41 +0100

    adding AQRReader tests

---
+++ tests/test_aqr.py
@@ -0,0 +1,38 @@
+from finance_data import AQRReader
+import pandas as pd
+
+def test_esg_efficient_frontier():
+    pass
+
+def test_bab_factors():
+    pass
+
+def test_factor_premia_century():
+    pass
+
+def test_commodites_long_run():
+    pass
+
+def test_momentum_indices():
+    pass
+
+def test_quality_sorted_portfolios():
+    pass
+
+def test_qmj_factors():
+    pass
+
+def test_quality_size_sorted_portfolios():
+    pass
+
+def test_hml_devil_factors():
+    pass
+
+def test_time_series_momentum():
+    pass
+
+def test_value_momentum_everywhere_factors():
+    pass
+
+def test_value_momentum_everywhere_portfolios():
+    pass
