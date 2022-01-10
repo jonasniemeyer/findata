96be1f4dbdb15ca0abe403f88cc676f6719074a9
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2022-01-10 16:24:13 +0100

    implementing wsj feed

--- rss.py
+++ rss.py
@@ -51,8 +51,40 @@ class RSSReader:
         return data
 
     @classmethod
-    def wsj(cls):
-        pass
+    def wsj(cls, source, timestamps=False) -> list:
+        url = "https://feeds.a.dj.com/rss/{}.xml"
+        if source not in ("opinion", "world", "us business", "markets", "technology", "lifestyle"):
+            raise ValueError("source has to be in ('opinion', 'world', 'us business', 'markets', 'technology', 'lifestyle')")
+        if source == "opinion":
+            url = url.format("RSSOpinion")
+        elif source == "world":
+            url = url.format("RSSWorldNews")
+        elif source == "us business":
+            url = url.format("WSJcomUSBusiness")
+        elif source == "markets":
+            url = url.format("RSSMarketsMain")
+        elif source == "technology":
+            url = url.format("RSSWSJD")
+        elif source == "lifestyle":        
+            url = url.format("RSSLifestyle")
+        
+        response = requests.get(url=url, headers=HEADERS).text
+        soup = BeautifulSoup(response, "lxml")
+        tags = soup.find_all("item")
+
+        data = []
+        for tag in tags:
+            dct = {}
+            dct["header"] = tag.find("title").text
+            dct["url"] = f"https://www.wsj.com/articles/{tag.find('guid').text}"
+            date =  tag.find("pubdate").text
+            date = int(pd.to_datetime(date).timestamp())
+            if not timestamps:
+                date = pd.to_datetime(date, unit="s").isoformat()
+            dct["date"] = date
+            data.append(dct)
+            
+        return data
 
     @classmethod
     def cnbc(cls):
