dc1aa9f300e1dce2a58b537f756868541582501e
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2022-03-18 12:29:53 +0100

    _

--- tests/test_msci.py
+++ tests/test_msci.py
@@ -21,12 +21,12 @@ def test_indices_list():
 def test_historical_data_default():
     end_date = dt.date.today().isoformat().replace("-","")
     data = MSCIReader(139245).historical_data()
-    df = data["data"]
     info = data["information"]
     assert info["index_code"] == 139245
     assert info["index_variant"] == "STRD"
     assert info["currency"] == "USD"
     assert info["url"] == f"https://app2.msci.com/products/service/index/indexmaster/getLevelDataForGraph?currency_symbol=USD&index_variant=STRD&start_date=19690101&end_date={end_date}&data_frequency=DAILY&index_codes=139245&normalize=False"
+    df = data["data"]
     assert all(isinstance(date, pd.Timestamp) for date in df.index)
     assert all(df[col].dtype == "float64" for col in df.columns)
 
@@ -60,7 +60,7 @@ def test_normalize():
     data = MSCIReader(139245, normalize=True).historical_data()["data"]
     assert all(isinstance(date, pd.Timestamp) for date in data.index)
     assert all(data[col].dtype == "float64" for col in data.columns)
-    assert data.iloc[0, 0] == 100
+    assert data.loc[data.index[0], "prices"] == 100
 
 def test_timestamps():
     data = MSCIReader(139245, timestamps=True).historical_data()["data"]
