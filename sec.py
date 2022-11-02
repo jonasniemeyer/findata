148a2fb13d22ca6740be291179802d9d2d0708e8
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2022-11-02 01:49:43 +0100

    change order of mutualfund dict items

--- sec.py
+++ sec.py
@@ -21,10 +21,10 @@ def sec_mutualfunds() -> list:
     items = requests.get("https://www.sec.gov/files/company_tickers_mf.json", headers=HEADERS).json()["data"]
     items = [
         {
-            "cik": item[0],
             "ticker": item[3],
-            "series_identifier": item[1],
-            "class_identifier": item[2],
+            "class_cik": item[2],
+            "series_cik": item[1],
+            "entity_cik": item[0]
         }
         for item in items
     ]
