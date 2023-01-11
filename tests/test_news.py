4032bc3c6550385a2e576c03011468464ba308c8
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-01-11 13:27:57 +0100

    add NasdaqNews tests

--- tests/test_news.py
+++ tests/test_news.py
@@ -52,3 +52,25 @@ def test_ft_news():
     assert len(articles) != 0
     for article in articles:
         assert isinstance(article["datetime"], int)
+
+def test_nasdaq_news():
+    articles = NasdaqNews.rss_feed("AAPL")
+    assert isinstance(articles, list)
+    assert len(articles) != 0
+
+    for article in articles:
+        assert isinstance(article["header"], str)
+        assert len(re.findall("[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}", article["datetime"])) == 1
+        assert isinstance(article["source"], str)
+        assert isinstance(article["categories"], list)
+        for category in article["categories"]:
+            assert isinstance(category, str)
+        assert isinstance(article["related_tickers"], list)
+        for ticker in article["related_tickers"]:
+            assert isinstance(ticker, str)
+        assert isinstance(article["url"], str)
+    
+    articles = NasdaqNews.rss_feed("AAPL", timestamps=True)
+    assert len(articles) != 0
+    for article in articles:
+        assert isinstance(article["datetime"], int)
