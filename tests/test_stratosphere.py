ecc0545981a6d98235f509de5197a77fc06b9f9d
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-02-16 17:31:06 +0100

    add investors test

--- tests/test_stratosphere.py
+++ tests/test_stratosphere.py
@@ -13,3 +13,26 @@ def test_fund_letters():
     letters = StratosphereReader.fund_letters(timestamps=True)
     for letter in letters:
         assert isinstance(letter["date"], int)
+
+def test_investors():
+    investors = StratosphereReader.investors()
+    for investor in investors:
+        assert isinstance(investor["name"], str)
+        assert isinstance(investor["manager"], str)
+        assert isinstance(investor["cik"], int)
+
+        statistics = investor["statistics"]
+        assert isinstance(statistics["market_value"], int)
+        assert isinstance(statistics["no_holdings"], int)
+        assert isinstance(statistics["purchased"], int)
+        assert isinstance(statistics["sold"], int)
+        assert isinstance(statistics["average_holding_period"], int)
+        assert statistics["concentration"] == round(statistics["concentration"], 4)
+        assert statistics["turnover"] == round(statistics["turnover"], 4)
+
+        portfolio = investor["portfolio"]
+        for security in portfolio:
+            assert isinstance(security["ticker"], str)
+            assert isinstance(security["cusip"], str)
+            assert security["type"] in ("Share", "Call", "Put")
+            assert security["weight"] == round(security["weight"], 6)
