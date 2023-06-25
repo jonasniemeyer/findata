fef6eb617c4b20b75d4dd96ba33c21e1e7a32aa5
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-06-25 13:13:22 +0200

    add CMEReader tests

--- tests/test_cme.py
+++ tests/test_cme.py
@@ -1,3 +1,37 @@
 from findata import CMEReader
+import numpy as np
 import pandas as pd
-import pytest
+
+class TestCMEReader:
+    @classmethod
+    def setup_class(cls):
+        cls.reader = CMEReader("WTI Crude Oil")
+
+    def test_attributes(self):
+        assert self.reader.commodity == "WTI Crude Oil"
+        assert self.reader.sector == "energy"
+        assert self.reader.group == "crude-oil"
+        assert self.reader.name == "west-texas-intermediate-wti-crude-oil-calendar-swap-futures"
+        assert self.reader.timestamps == False
+
+    def test_data(self):
+        dct = self.reader.read()
+        assert len(dct) in (4,5)
+        for date, df in dct.items():
+            assert isinstance(date, str) and pd.to_datetime(date)
+            assert isinstance(df, pd.DataFrame)
+            assert all(isinstance(date, pd.Timestamp) for date in df.index)
+            assert df.index.is_unique is True
+            for col in df.columns:
+                assert isinstance(df[col].dtype, (type(np.dtype("float64")), type(np.dtype("int64"))))
+
+    def test_timestamps_parameter(self):
+        dct = CMEReader("WTI Crude Oil", timestamps=True).read()
+        assert len(dct) in (4,5)
+        for date, df in dct.items():
+            assert isinstance(date, int) and pd.to_datetime(date, unit="s")
+            assert isinstance(df, pd.DataFrame)
+            assert all(isinstance(date, int) for date in df.index)
+            assert df.index.is_unique is True
+            for col in df.columns:
+                assert isinstance(df[col].dtype, (type(np.dtype("float64")), type(np.dtype("int64"))))
