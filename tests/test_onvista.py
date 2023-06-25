a3c293329f46563bb493c092db41a4019772910f
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-06-25 15:40:10 +0200

    add OnvistaFundReader tests

--- tests/test_onvista.py
+++ tests/test_onvista.py
@@ -78,3 +78,93 @@ class TestOnvistaBondReader:
         assert profile["in_default"] is False
         assert profile["perpetual"] is False
         assert profile["callable"] is False
+
+
+class TestOnvistaFundReader:
+    @classmethod
+    def setup_class(cls):
+        cls.reader = OnvistaFundReader("LU0323578657")
+
+    def test_attributes(self):
+        assert self.reader.isin == "LU0323578657"
+        assert self.reader.issuer == "Flossbach von Storch Invest S.A."
+        assert self.reader.name == "Flossbach von Storch Multiple Opportunities R"
+
+    def test_benchmark_indices(self):
+        assert self.reader.benchmark_indices() == [{"name": "MSCI WORLD INDEX (GDTR, UHD)", "url": "https://www.onvista.de/index/MSCI-WORLD-INDEX-GDTR-UHD-Index-12221463", "id": 12221463}]
+
+    def test_exchanges(self):
+        exchanges = self.reader.exchanges()
+        for item in exchanges:
+            assert isinstance(item["name"], str)
+            assert isinstance(item["abbr"], str)
+            assert isinstance(item["code"], str)
+            assert isinstance(item["dataset_id"], int)
+            assert isinstance(item["country"], str)
+            assert isinstance(item["currency"], str)
+            assert isinstance(item["volume"], int)
+            assert isinstance(item["4_week_volume"], int)
+            assert isinstance(item["unit"], str)
+
+    def test_historical_data(self):
+        data = self.reader.historical_data()
+        info = data["information"]
+        assert info["instrument_id"] == 18085585
+        assert info["dataset_id"] == 120555483
+        assert info["start"] == "2011-10-31"
+        assert isinstance(info["end"], str) and pd.to_datetime(info["end"])
+        assert info["exchange"]["name"] == "gettex"
+        assert info["exchange"]["code"] == "_TRO"
+        assert info["exchange"]["country"] == "DE"
+        assert info["currency"] == "EUR"
+
+        df = data["data"]
+        assert all(isinstance(date, pd.Timestamp) for date in df.index)
+        assert df.index.is_unique is True
+        for col in df.columns:
+            assert isinstance(df[col].dtype, (type(np.dtype("float64")), type(np.dtype("int64"))))
+
+    def test_managers(self):
+        assert self.reader.managers == ["Dr. Bert Flossbach"]
+
+    def test_morningstar_rating(self):
+        rating = self.reader.morningstar_rating()
+        for key in rating:
+            assert key in ("bond_style", "equity_style", "rating", "sustainability")
+        assert isinstance(rating["bond_style"], int)
+        assert isinstance(rating["equity_style"], int)
+        for key, data in rating["rating"].items():
+            assert key in ("1y", "3y", "5y", "10y")
+            assert isinstance(data, int)
+        assert isinstance(rating["sustainability"], str)
+
+    def test_profile(self):
+        profile = self.reader.profile()
+        assert isinstance(profile["aum"], int)
+        assert profile["emission_date"] == "2007-10-23"
+        assert profile["currency"] == "EUR"
+        assert profile["custodian_bank"] == "DZ Privatbank S.A."
+        assert profile["custodian_country"]["name"] == "Luxemburg"
+        assert profile["custodian_country"]["abbr"] == "LU"
+        assert profile["intitial_charge"] == 0.05
+        assert profile["ter"] == 0.0162
+        assert profile["management_fee"] == 0.0153
+        assert profile["custodian_fee"] == 0.0009
+
+    def test_reports(self):
+        reports = self.reader.reports()
+        for key, url in reports.items():
+            assert isinstance(key, str)
+            assert url.startswith("https://mediaproxy.mdgms.com/download.html?docId=")
+
+    def test_sector_breakdown(self):
+        breakdown = self.reader.sector_breakdown()
+        for key, percent in breakdown.items():
+            assert key in ("Konsumgüter zyklisch", "Finanzen", "Informationstechnologie", "Basiskonsumgüter", "Gesundheitswesen", "Industrie", "Telekomdienste", "Rohstoffe")
+            assert round(percent, 4) == percent
+
+    def test_top_holdings(self):
+        holdings = self.reader.top_holdings()
+        for item in holdings:
+            assert isinstance(item["name"], str)
+            assert round(item["percentage"], 4) == item["percentage"]
