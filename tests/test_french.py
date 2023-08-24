12add9c7ca1771a9eba50904fc8531f5c02259b3
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-08-24 12:59:55 +0200

    add industry portfolios sic code data tests

--- tests/test_french.py
+++ tests/test_french.py
@@ -7,6 +7,8 @@ def test_datasets():
     datasets = FrenchReader.datasets()
     assert isinstance(datasets, list)
     assert len(datasets) == 297
+    for item in datasets:
+        assert isinstance(item, str)
 
 
 def test_retrieval():
@@ -19,7 +21,7 @@ def test_retrieval():
         assert data["Main"][col].dtype == np.float64
 
 
-def test_retrieval_sorted_portfolios():
+def test_sorted_portfolios():
     data = FrenchReader("Portfolios_Formed_on_BE-ME").read()
     assert all(
         key in (
@@ -40,6 +42,37 @@ def test_retrieval_sorted_portfolios():
         for col in data[key]:
             assert data[key][col].dtype in (np.int64, np.float64)
 
+
+def test_industry_portfolios():
+    data = FrenchReader("49_Industry_Portfolios").read()
+    assert all(
+        key in (
+            "Average Value Weighted Returns Monthly",
+            "Average Equal Weighted Returns Monthly",
+            "Average Value Weighted Returns Annual",
+            "Average Equal Weighted Returns Annual",
+            "Number of Firms in Portfolios",
+            "Average Firm Size",
+            "Sum of BE / Sum of ME",
+            "Value-Weighted Average of BE/ME",
+            "industries"
+        )
+        for key in data
+    )
+    for key in data:
+        if key == "industries":
+            continue
+        assert all(isinstance(date, pd.Timestamp) for date in data[key].index)
+        for col in data[key]:
+            assert data[key][col].dtype in (np.int64, np.float64)
+
+    for col in data["Average Value Weighted Returns Monthly"].columns:
+        assert isinstance(data["industries"][col]["name"], str)
+        assert len(data["industries"][col]["sic_codes"]) > 0
+        for item in data["industries"][col]["sic_codes"]:
+            assert isinstance(item["name"], str)
+            assert isinstance(item["lower"], int)
+            assert isinstance(item["upper"], int)
 
 def test_timestamps():
     data = FrenchReader("Portfolios_Formed_on_BE-ME", timestamps=True).read()
