419f7abdbb2a1a5ba9d8e02a4fa1eab6faa5dd4a
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2024-07-03 12:46:18 +0200

    update requests headers

--- findata/utils.py
+++ findata/utils.py
@@ -15,7 +15,7 @@ HEADERS = {
     "Upgrade-Insecure-Requests": "1",
     "User-Agent": (
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-        "(KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36"
+        "(KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
     )
 }
 HEADERS_FAKE = deepcopy(HEADERS)
